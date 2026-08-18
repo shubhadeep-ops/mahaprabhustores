d0b0202ee42b0ee874842613e895db18eb616538
shubhadeep-ops <shubhadeepmistri@gmail.com>
2026-08-18 22:27:10 +0530

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDTechwhJPt4zJoKdutEu4TvGNmpCPBSs4",
+  authDomain: "mahaprabhustores-65045.firebaseapp.com",
+  projectId: "mahaprabhustores-65045",
+  storageBucket: "mahaprabhustores-65045.firebasestorage.app",
+  messagingSenderId: "1013233682498",
+  appId: "1:1013233682498:web:56fe1ef187c5af2b7445db",
+  measurementId: "G-0C5X7NPV3R"
+});
+
+const messaging = firebase.messaging();
